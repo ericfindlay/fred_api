aa5458ba3ae2fa2e33f7397587ee98b41b908103
Eric Findlay <e.findlay@protonmail.ch>
2024-04-11 13:56:50 +0900

    Improved main documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -17,14 +17,17 @@ use {
 # tokio_test::block_on(async {
 // Set the path to the cache. The cache stores the response bytes as is, keyed by
 // the `RequestSpec`.
-let db: sled::Db = sled::open("../fred_cache/db").unwrap();
+let cache: sled::Db = sled::open("../fred_cache/db").unwrap();
 
-// The spec string can be taken from the FRED API docs.
+// The spec string can be taken from the FRED API docs. `req_spec` uniquely
+// identifies the request and is used as a key in the cache. `req` is a
+// `hyper::Request`.
 let (req_spec, req) = build_request("series/observations?series_id=CPGRLE01AUQ657N").unwrap();
 
-// Determine whether to request from the cache and/or request from FRED. Successful FRED
-// responses are always cached. Responses with other than Ok status will return an error.
-let bytes = send_request(&req_spec, req, true, false, &db).await.unwrap();
+// The boolean values determine whether to request from the cache and/or request from
+// FRED. Successful FRED responses are always cached. Responses with other than HTTP
+// status `Ok` status will return an error.
+let bytes = send_request(&req_spec, req, true, false, &cache).await.unwrap();
 
 // Consult the FRED API docs XML responses to determine the fields to be captured
 // from the bytes. The fields must be specified in the same order they appear in the
